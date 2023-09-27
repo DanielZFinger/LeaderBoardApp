4b40e927f2a14bb6e5757019392449ade8366c0a
Daniel Finger <64750813+DanielZFinger@users.noreply.github.com>
2023-09-27 17:30:00 -0400

    front end added ability to fetch simple data from backend using the api and display it

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+
+  const [backendData, setBackendData]=useState([{}]);
+
+  useEffect(() =>{
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
     <div>
       <button>test button</button>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
     </div>
   );
 }
