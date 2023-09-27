bd16793294efd049489c8a56893d538c31d140bc
Daniel Finger <64750813+DanielZFinger@users.noreply.github.com>
2023-09-27 16:44:43 -0400

    initial node creation. Added react for frontend and node for backend API and server side

---
+++ server/index.js
@@ -0,0 +1,10 @@
+//imports
+const express = require('express');
+
+const app = express();
+
+app.get('/api', (request, response)=>{
+    response.json({"users:": ["userOne", "userTwo", "userThree"]})
+});
+
+app.listen(5000, ()=>console.log('Server available on localhost 5000'));
