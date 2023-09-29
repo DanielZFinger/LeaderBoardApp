467adafeef9b30b6aedfaec8fb2236abd7e77ec2
Daniel Finger <64750813+DanielZFinger@users.noreply.github.com>
2023-09-29 17:01:37 -0400

    connected to mongoose and mondodb connection dynamic successfull

--- server/index.js
+++ server/index.js
@@ -1,10 +1,53 @@
 //imports
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = 5000;
 
-app.get('/api', (request, response)=>{
-    response.json({"users": ["userOne", "userTwo", "userThree"]})
+//mongoDB connection
+const dbURI = 'mongodb+srv://danielzanefinger:f8hi0l9uLRAeXbrn@cluster0.arhghcr.mongodb.net/leaderboards?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => console.log("connected to db"))
+    .catch((err) => console.log(err));
+
+app.use(express.json());
+
+//schema
+const leaderboardSchema = new mongoose.Schema({
+  name: String,
+  clicks: Number,
 });
 
-app.listen(5000, ()=>console.log('Server available on localhost 5000'));
+const Leaderboard = mongoose.model('Leaderboard', leaderboardSchema, 'leaderboards');
+
+module.exports = Leaderboard;
+
+  
+  // Create an API endpoint to save user data
+  // Create an API endpoint to save user data
+app.post('/api/saveUserData', async (req, res) => {
+    try {
+      const { name, clicks } = req.body;
+  
+      // Create a new leaderboard data document
+      const leaderboardData = new Leaderboard({
+        name,
+        clicks,
+      });
+  
+      // Save the data to MongoDB
+      await leaderboardData.save();
+  
+      res.status(200).json({ message: 'Leaderboard data saved successfully' });
+    } catch (error) {
+      console.error('Error saving leaderboard data:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+    
+  // Start the server
+  app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+  });
